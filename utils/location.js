aa176643cce4d6bbdb1a9444ae6d7afeaa462d63
gpfdeveloper <pengfei.gao.2014@gmail.com>
2021-11-18 09:29:48 +0800

    using position stack api to get latitude and longitude for a given address

--- utils/location.js
+++ utils/location.js
@@ -1,8 +1,30 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = process.env.MAP_KEY;
+
 async function getCoordsForAddress(address) {
-  return {
-    lat: 40.7484474,
-    lng: -73.9871516,
-  };
+  const coordinates = {lat: 0, lng: 0}
+  const response = await axios.get(
+    `http://api.positionstack.com/v1/forward?access_key=${API_KEY}&query=${encodeURIComponent(
+      address
+    )}`
+  );
+  const data = response.data?.data;
+
+  if (!data || data.length === 0) {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+    throw error;
+  }
+
+  coordinates.lat = data[0].latitude;
+  coordinates.lng = data[0].longitude;
+
+  return coordinates;
 }
 
 module.exports = getCoordsForAddress;
